86dcc6124ad1b338ecab14811fc178708ec16a6d
Nikolaaa777 <151403198+Nikolaaa777@users.noreply.github.com>
2025-11-14 18:31:10 +0100

    Update models.py

--- bekend/accounts/models.py
+++ bekend/accounts/models.py
@@ -1,12 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     is_walker = models.BooleanField(default=False)  # True=šetač pasa, False=vlasnik
-    has_notifications_on = models.BooleanField(default=False)  #da li je pretplaćen na obavijesti
+    has_notifications_on = models.BooleanField(default=False)  # da li je pretplaćen na obavijesti
 
     def __str__(self):
         return f"{self.user.email} | walker={self.is_walker} | notif={self.has_notifications_on}"
-    
+
+
+#AUTOMATSKA KREACIJA PROFILE-A ZA SVAKI NOVI USER
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
