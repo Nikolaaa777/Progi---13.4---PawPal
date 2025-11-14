dd60268d5df86e3a8e2d3ec9365619b3c9e7d847
Nikolaaa777 <151403198+Nikolaaa777@users.noreply.github.com>
2025-11-14 03:58:26 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,20 +6,21 @@ import Register from "./pages/Register";
 import { api } from "./api/client";
 
 export default function App() {
-	const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
 
-	useEffect(() => {
-		api
-			.me()
-			.then(setUser)
-			.catch(() => setUser(null));
-	}, []);
+  // na mountu provjeri tko je logiran
+  useEffect(() => {
+    api
+      .me()
+      .then(setUser)        // api.me() vraća user ili null
+      .catch(() => setUser(null));
+  }, []);
 
-	return (
-		<Routes>
-			<Route path="/" element={<Home user={user} />} />
-			<Route path="/login" element={<Login />} />
-			<Route path="/register" element={<Register />} />
-		</Routes>
-	);
+  return (
+    <Routes>
+      <Route path="/" element={<Home user={user} setUser={setUser} />} />
+      <Route path="/login" element={<Login setUser={setUser} />} />
+      <Route path="/register" element={<Register setUser={setUser} />} />
+    </Routes>
+  );
 }
