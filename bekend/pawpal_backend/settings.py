6646f3b0a61d1bc9c7d657a959b119dd6a9d25b4
Nikola Ivkovic <ni008207243@fer.hr>
2026-01-11 16:27:55 +0100

    nova db

--- bekend/pawpal_backend/settings.py
+++ bekend/pawpal_backend/settings.py
@@ -111,7 +111,7 @@ CORS_ALLOWED_ORIGINS = [
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost:5173", "http://127.0.0.1:5173","http://localhost:8000",
-    "http://127.0.0.1:8000", "progi-13-4-pawpal.onrender.com", "https://progi-13-4-pawpal.onrender.com", "https://pawpal-front.onrender.com","pawpal-front.onrender.com",]
+    "http://127.0.0.1:8000", "https://progi-13-4-pawpal.onrender.com", "https://pawpal-front.onrender.com",]
 SESSION_COOKIE_SAMESITE = "None"
 CSRF_COOKIE_SAMESITE = "None"
 SESSION_COOKIE_SECURE = True
