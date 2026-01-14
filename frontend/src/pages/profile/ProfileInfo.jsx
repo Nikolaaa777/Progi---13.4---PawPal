2130a4c05ab60ec37f5973823c2b7fac5bbc2081
Zvonimir <zvonimir.schonwald@fer.hr>
2026-01-14 15:24:14 +0100

    Dodana profile stranica

---
+++ frontend/src/pages/profile/ProfileInfo.jsx
@@ -0,0 +1,34 @@
+import "../../styles/all.css";
+
+export default function ProfileInfo() {
+	return (
+		<div className="app">
+			<aside className="sidebar">
+				<div className="back-arrow">←</div>
+
+				<div className="profile">
+					<div className="avatar"></div>
+					<div className="username">ImePrezime vlasnika</div>
+				</div>
+
+				<nav className="menu">
+					<a className="active">Moje informacije</a>
+					<a>Moji ljubimci</a>
+					<a>Moje rezervacije</a>
+				</nav>
+			</aside>
+
+			<main className="content">
+				<h1>Moje informacije</h1>
+				<div className="center-text">SADRŽAJ</div>
+
+				<button className="edit-btn">
+					Uredi profil
+					<span className="icon-btn">
+						<img src="/images/edit.png" alt="edit" />
+					</span>
+				</button>
+			</main>
+		</div>
+	);
+}
