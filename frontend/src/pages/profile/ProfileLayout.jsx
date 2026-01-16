3df83044bd7425e3dcea888c5b1a5d048371f127
Brca04 <bruno.cavor@gmail.com>
2026-01-16 03:56:51 +0100

    Gazenje

--- frontend/src/pages/profile/ProfileLayout.jsx
+++ frontend/src/pages/profile/ProfileLayout.jsx
@@ -2,24 +2,25 @@ import { NavLink, Outlet, useNavigate } from "react-router-dom";
 import "../../styles/profileMenu.css";
 
 export default function ProfileLayout() {
-	const nav = useNavigate();
+  const nav = useNavigate();
 
-	return (
-		<div className="app">
-			<aside className="sidebar">
-				<div className="back-arrow" onClick={() => nav("/")}>
-					←
-				</div>
-
-				<nav className="menu">
-					<NavLink end to="/profile">Moje informacije</NavLink>
-					<NavLink to="/profile/ljubimci">Moji ljubimci</NavLink>
-					<NavLink to="/profile/rezervacije">Moje rezervacije</NavLink>
-					<NavLink to="/profile/termini">Moji termini</NavLink>
-					<NavLink to="/profile/zahtjevi">Zahtjevi za šetnju</NavLink>
-				</nav>
-			</aside>
-			<Outlet />
-		</div>
-	);
+  return (
+    <div className="app">
+      <aside className="sidebar">
+        <nav className="menu">
+          <div className="back-arrow" onClick={() => nav("/")}>
+            ←
+          </div>
+          <NavLink end to="/profile">
+            Moje informacije
+          </NavLink>
+          <NavLink to="/profile/ljubimci">Moji ljubimci</NavLink>
+          <NavLink to="/profile/rezervacije">Moje rezervacije</NavLink>
+          <NavLink to="/profile/termini">Moji termini</NavLink>
+          <NavLink to="/profile/zahtjevi">Moji zahtjevi</NavLink>
+        </nav>
+      </aside>
+      <Outlet />
+    </div>
+  );
 }
